b0d23fc2bc9a40144086cfadb1f6b8368ceafe7c
Vitor Ricardo <vitorricardo@outlook.com>
2022-03-07 15:08:00 -0300

    fix: export types

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,2 +1,3 @@
 import CobrancaBancaria from './cobranca_bancaria';
+export { TRetornoCNAB, TBoletoCNAB, TCabecalhoCNAB, TRodapeCNAB } from './arquivos_retorno/type';
 export default CobrancaBancaria;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import CobrancaBancaria from './cobranca_bancaria';
+export { TRetornoCNAB, TBoletoCNAB, TCabecalhoCNAB, TRodapeCNAB } from './arquivos_retorno/type';
 
 export default CobrancaBancaria
